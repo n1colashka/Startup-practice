f1e48763980e14ea5de800c5de54d475c2c39465
n1colashka <deckards2505@gmail.com>
2020-02-04 16:37:03 +0600

    clients section stylized

--- js/main.js
+++ js/main.js
@@ -1,17 +1,38 @@
 $(document).ready(function(){
-  $(".owl-carousel").owlCarousel({
-    items: 4
-  });
+  $(".about-us__slider").owlCarousel({
+        loop:true, //Зацикливаем слайдер
+        autoplay:true, //Автозапуск слайдера
+        smartSpeed:1000, //Время движения слайда
+        autoplayTimeout:2000, //Время смены слайда
+        responsive:{ //Адаптация в зависимости от разрешения экрана
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:4
+            }
+        }
+    });
+
+    $(".clients-slider").owlCarousel({
+        loop:true, //Зацикливаем слайдер
+        margin:10, //Отступ от картино если выводите больше 1
+        autoplay:true, //Автозапуск слайдера
+        smartSpeed:2000, //Время движения слайда
+        autoplayTimeout:4000, //Время смены слайда
+        responsive:{ //Адаптация в зависимости от разрешения экрана
+            0:{
+                items:3
+            },
+            600:{
+                items:4
+            },
+            1000:{
+                items:5
+            }
+        }
+    });
 });
-
-if($(window).width() < 1150){
-  $(".owl-carousel").owlCarousel({
-    items: 3
-  });
-}
-
-if($(window).width() < 850){
-  $(".owl-carousel").owlCarousel({
-    items: 2
-  });
-}
