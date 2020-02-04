3201b8bf3a58eb092eaa9ea16bc3de166556c00f
n1colashka <deckards2505@gmail.com>
2020-02-04 18:51:43 +0600

    Finish!

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
         loop:true, //Зацикливаем слайдер
         autoplay:true, //Автозапуск слайдера
         smartSpeed:1000, //Время движения слайда
-        autoplayTimeout:2000, //Время смены слайда
+        autoplayTimeout:4000, //Время смены слайда
         responsive:{ //Адаптация в зависимости от разрешения экрана
             0:{
                 items:1
